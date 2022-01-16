1dbcfa81b961b8e28ce21166424f7e39451197d2
antoine-marie <antoine.marie@ens-lyon.fr>
2022-01-16 15:21:25 +0100

    First version of the website!

--- build-site.el
+++ build-site.el
@@ -20,7 +20,21 @@
 (setq org-html-validation-link nil            ;; Don't show validation link
       org-html-head-include-scripts nil       ;; Use our own scripts
       org-html-head-include-default-style nil ;; Use our own styles
-      org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />")
+      org-html-head "<link rel=\"stylesheet\" href=\"css/org.css\" />")
+
+(setq amarie/html-preamble (concat
+"<ul class='nav'>
+       <li><a href='index.html'>About</a></li>
+       <li><a href='resume.html'>Resume</a></li>
+       <li><a href='publications.html'>Publications</a></li>
+       <li><a href='bookmarks.html'>Bookmarks</a></li>
+ </ul>
+ "))
+
+(setq amarie/html-postamble (concat "<div class='footer'>
+                                      Last updated %C. <br>
+                                      Built with %c.
+                                      </div>"))
 
 ;; Define the publishing project
 (setq org-publish-project-alist
@@ -32,9 +46,22 @@
              :publishing-directory "./public"
              :with-author nil           ;; Don't include author name
              :with-creator t            ;; Include Emacs and Org versions in footer
-             :with-toc t                ;; Include a table of contents
+             :with-toc nil              ;; Don't include a table of contents
              :section-numbers nil       ;; Don't include section numbers
-             :time-stamp-file nil)))    ;; Don't include time stamp in file
+             :time-stamp-file nil       ;; Don't include time stamp in file
+	     :html-preamble amarie/html-preamble
+	     :html-postamble amarie/html-postamble
+	     :auto-sitemap t                ; Generate sitemap.org automatically...
+	     :sitemap-filename "sitemap.org"  ; ... call it sitemap.org (it's the default)...
+	     :sitemap-title "Sitemap"         ; ... with title 'Sitemap'.
+	     ) 
+       (list "org-site:static"
+	:base-directory "./content"
+	:base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
+	:publishing-directory "./public"
+	:recursive t
+	:publishing-function 'org-publish-attachment
+	)))
 
 ;; Generate the site output
 (org-publish-all t)
